9c5e0262dae24593f655ff73da6e259955da6b80
kmmch <kmmch.tech@gmail.com>
2023-04-17 23:02:02 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -142,3 +142,46 @@ console.log("-------------------------------------------------");
 // nameの後に=を書いて設定したい値を書く
 const sayHello = (name="Guest") => console.log(`こんにちは！${name}さん！`);
 sayHello("まさや");
+
+
+
+/**
+ * スプレッド構文
+ */
+console.log("------------------------------------------");
+console.log("               スプレッド構文               ");
+console.log("------------------------------------------");
+// 配列の展開
+// → 配列の中身を順番に処理してくれる
+const arr1 = [1,2];
+console.log(arr1);
+console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
+
+// 一つにまとめる
+const arr2 = [1,2,3,4,5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6.push(100);
+console.log(arr6);
+console.log(arr4);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+// =で代入すると参照元も更新される。
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
